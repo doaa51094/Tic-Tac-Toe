525812597a55f0580b4265b4003f62b56264311b
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-07-09 00:25:18 +0300

    handle edit player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 
 export const Player = ({ name, symbol }) => {
   const [isEditting, setIsEditting] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
   function handleEditClick() {
     setIsEditting((editing)=> !editing )  //true
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
   return (
     <li>
       <span className="player">
         {!isEditting ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" required value={name} />
+          <input type="text" required value={playerName} onChange={handleChange}/>
         )}
         <span className="player-symbol">{symbol}</span>{" "}
       </span>

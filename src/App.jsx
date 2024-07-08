df7349fe3b95ea41fd1738a025d4df898fc0da62
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-07-08 23:42:54 +0300

    create component player

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Player } from "./components/Player";
-
-function App() {
+import React from 'react'
+import {Player} from'./components/Player.jsx'
+const App = () => {
   return (
     <main>
-      dxgfchjkl;,
-      <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
-        </ol>
-      </div>
-    </main>
-  );
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
+      </ol>
+    </div>
+  </main>  )
 }
 
-export default App;
+export default App
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Player = ({ name, symbol }) => {
   const [isEditting, setIsEditting] = useState(false);
+  function handleEditClick() {
+    setIsEditting((editing)=> !editing )  //true
+  }
   return (
     <li>
       <span className="player">
-        {isEditting ? (
+        {!isEditting ? (
           <span className="player-name">{name}</span>
         ) : (
-          <input type="text" />
+          <input type="text" required value={name} />
         )}
         <span className="player-symbol">{symbol}</span>{" "}
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditting?'Save':'Edit'}</button>
     </li>
   );
 };
